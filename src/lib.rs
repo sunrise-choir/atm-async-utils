76174dd255d88653b9bd71a4c1790455c181086a
AljoschaMeyer <mail@aljoscha-meyer.de>
2018-03-09 16:47:09 +0100

    Update to futures 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,9 @@
 //! Utilities for working with `futures::Sink` and `futures::Stream`.
 #![deny(missing_docs)]
 
-#[macro_use]
-extern crate futures;
+extern crate futures_core;
+extern crate futures_sink;
+extern crate futures_channel;
 extern crate quickcheck;
 
-#[cfg(test)]
-extern crate void;
-#[cfg(test)]
-extern crate rand;
-
-pub mod test_sink;
-pub mod test_stream;
 pub mod test_channel;
-pub mod sink_futures;
--- src/test_channel.rs
+++ src/test_channel.rs
@@ -1,185 +1,91 @@
-//! An in-memory channel, primarily for testing purposes. Like futures::unsync::mpsc, but:
-//! - single-producer
-//! - allocates a vector of the full capacity in `new`
-//! - sender can trigger an error on the receiver
-//! - allow an arbitrary SinkError type, for use with `TestSink`
+//! An in-memory channel for testing purposes. Allows sending items and errors to a receiver.
 
-use std::marker::PhantomData;
-use std::rc::Rc;
-use std::cell::RefCell;
-use std::collections::VecDeque;
+use futures_core::{Stream, Poll, Async, Never};
+use futures_core::task::Context;
+use futures_sink::Sink;
+use futures_channel::mpsc::{channel, Sender, Receiver};
 
-use futures::{Sink, Stream, StartSend, Poll, AsyncSink, Async};
-use futures::task::{Task, current};
-
-/// Creates a new test channel with the given capacity, and returns a Sink for
-/// writing to it, and a Stream for reading from it.
+/// Create a test channel of a given capacity.
 ///
-///  I: Item, SE: SinkError, E: Error (of the Receiver)
+/// `I` is the type of items sent over the channel, `E` is the type of errors sent over the channel.
 ///
 /// # Panics
-/// Panics if capacity is `0`.
-pub fn test_channel<I, SE, E>(capacity: usize) -> (Sender<I, SE, E>, Receiver<I, SE, E>) {
+/// Panics if the given capacity is 0.
+pub fn test_channel<I, E>(capacity: usize) -> (TestSender<I, E>, TestReceiver<I, E>) {
     if capacity == 0 {
-        panic!("Invalid test channel capacity.");
+        panic!("TestChannel must have capacity greater than 0")
     }
-
-    let data: VecDeque<I> = VecDeque::with_capacity(capacity);
-
-    let channel = Rc::new(RefCell::new(TestChannel {
-                                           data,
-                                           task: None,
-                                           error: None,
-                                           end_of_stream: false,
-                                           sink_error: PhantomData,
-                                       }));
-
-    (Sender(Rc::clone(&channel)), Receiver(channel))
+    let (sender, receiver) = channel(capacity - 1);
+    (TestSender::new(sender), TestReceiver::new(receiver))
 }
 
-struct TestChannel<I, SE, E> {
-    data: VecDeque<I>,
-    task: Option<Task>,
-    error: Option<E>,
-    end_of_stream: bool,
-    sink_error: PhantomData<SE>,
-}
-
-impl<I, SE, E> TestChannel<I, SE, E> {
-    fn unpark(&mut self) {
-        self.task.take().map(|task| task.notify());
-    }
-}
-
-/// Send data to a TestChannel.
+/// The transmission end of a test channel.
 ///
-/// If there is no space in the channel to hold the item, the current task is
-/// parked and notified once space becomes available.
-pub struct Sender<I, SE, E>(Rc<RefCell<TestChannel<I, SE, E>>>);
+/// This is built upon `futures::channel::mpcs::sender` and panics if the underlying `Sender` emits
+/// an error.
+pub struct TestSender<I, E>(Sender<Result<I, E>>);
 
-impl<I, SE, E> Sender<I, SE, E> {
-    /// Triggers an error on the Receiver.
-    pub fn error(&mut self, e: E) {
-        let mut c = self.0.borrow_mut();
-        c.error = Some(e);
-        c.unpark();
+impl<I, E> TestSender<I, E> {
+    fn new(sender: Sender<Result<I, E>>) -> TestSender<I, E> {
+        TestSender(sender)
     }
 }
 
-impl<I, SE, E> Drop for Sender<I, SE, E> {
-    fn drop(&mut self) {
-        let mut c = self.0.borrow_mut();
-        c.end_of_stream = true;
-        c.unpark();
-    }
-}
+impl<I, E> Sink for TestSender<I, E> {
+    type SinkItem = Result<I, E>;
+    type SinkError = Never;
 
-impl<I, SE, E> Sink for Sender<I, SE, E> {
-    type SinkItem = I;
-    type SinkError = SE;
-
-    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
-        let mut c = self.0.borrow_mut();
-
-        // Can't write anything, so park.
-        if c.data.len() == c.data.capacity() {
-            c.task = Some(current());
-            return Ok(AsyncSink::NotReady(item));
-        } else {
-            c.data.push_back(item);
-            c.unpark();
-            return Ok(AsyncSink::Ready);
+    fn poll_ready(&mut self, cx: &mut Context) -> Poll<(), Self::SinkError> {
+        match self.0.poll_ready(cx) {
+            Err(err) => panic!("TestSender got a send error: {:?}", err),
+            Ok(non_err) => Ok(non_err),
         }
     }
 
-    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
-        Ok(Async::Ready(()))
+    fn start_send(&mut self, item: Self::SinkItem) -> Result<(), Self::SinkError> {
+        match self.0.start_send(item) {
+            Err(err) => panic!("TestSender got a send error: {:?}", err),
+            Ok(non_err) => Ok(non_err),
+        }
     }
 
-    fn close(&mut self) -> Poll<(), Self::SinkError> {
-        self.0.borrow_mut().end_of_stream = true;
-        Ok(Async::Ready(()))
+    fn poll_flush(&mut self, cx: &mut Context) -> Poll<(), Self::SinkError> {
+        match self.0.poll_flush(cx) {
+            Err(err) => panic!("TestSender got a send error: {:?}", err),
+            Ok(non_err) => Ok(non_err),
+        }
+    }
+
+    fn poll_close(&mut self, cx: &mut Context) -> Poll<(), Self::SinkError> {
+        match self.0.poll_close(cx) {
+            Err(err) => panic!("TestSender got a send error: {:?}", err),
+            Ok(non_err) => Ok(non_err),
+        }
     }
 }
 
-/// Receive data from a TestChannel.
-///
-/// If there is no data in the channel to read from, the current task is parked
-/// and notified once data becomes available.
-pub struct Receiver<I, SE, E>(Rc<RefCell<TestChannel<I, SE, E>>>);
+/// The receiving end of a test channel.
+pub struct TestReceiver<I, E>(Receiver<Result<I, E>>);
 
-impl<I, SE, E> Drop for Receiver<I, SE, E> {
-    fn drop(&mut self) {
-        self.0.borrow_mut().unpark();
+impl<I, E> TestReceiver<I, E> {
+    fn new(receiver: Receiver<Result<I, E>>) -> TestReceiver<I, E> {
+        TestReceiver(receiver)
     }
 }
 
-impl<I, SE, E> Stream for Receiver<I, SE, E> {
+impl<I, E> Stream for TestReceiver<I, E> {
     type Item = I;
     type Error = E;
 
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let mut c = self.0.borrow_mut();
-
-        match c.data.pop_front() {
-            None => {
-                if c.end_of_stream {
-                    return Ok(Async::Ready(None));
-                }
-
-                let e = c.error.take();
-                if e.is_some() {
-                    c.unpark();
-                    return Err(e.unwrap());
-                }
-
-                c.task = Some(current());
-                return Ok(Async::NotReady);
-            }
-            Some(item) => {
-                c.unpark();
-                return Ok(Async::Ready(Some(item)));
-            }
+    fn poll_next(&mut self, cx: &mut Context) -> Poll<Option<Self::Item>, Self::Error> {
+        match self.0.poll_next(cx) {
+            Ok(Async::Ready(Some(Ok(item)))) => Ok(Async::Ready(Some(item))),
+            Ok(Async::Ready(Some(Err(err)))) => Err(err),
+            Ok(Async::Ready(None)) => Ok(Async::Ready(None)),
+            Ok(Async::Pending) => Ok(Async::Pending),
+            Err(_) => unreachable!(),
         }
     }
 }
 
-#[cfg(test)]
-mod tests {
-
-    use quickcheck::{QuickCheck, StdGen};
-    use futures::Future;
-    use futures::stream::iter_ok;
-    use void::Void;
-    use rand;
-
-    use super::*;
-
-    #[test]
-    fn test_error() {
-        let (mut sender, receiver) = test_channel::<u8, u8, u8>(1);
-        sender.error(42);
-        assert_eq!(receiver.into_future().wait().err().unwrap().0, 42);
-    }
-
-    #[test]
-    fn test_success() {
-        let rng = StdGen::new(rand::thread_rng(), 100);
-        let mut quickcheck = QuickCheck::new().gen(rng).tests(10000);
-        quickcheck.quickcheck(success as fn(usize, Vec<u8>) -> bool);
-    }
-
-    fn success(buf_size: usize, data: Vec<u8>) -> bool {
-        let data_copy = data.clone();
-        if data.len() == 0 {
-            return true;
-        }
-        let (sender, receiver) = test_channel::<u8, Void, Void>(buf_size + 1);
-        let s = sender.send_all(iter_ok(data));
-
-        let r = receiver.collect();
-        let (received, _) = r.join(s).wait().unwrap();
-
-        return received == data_copy;
-    }
-}
+// TODO make sure TestSender and TestReceiver are thread-safe
